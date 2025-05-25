01a7b6d2832930ff6118960b62214a98e531abac
mmyonaa <apddfhsajrwk@gmail.com>
2025-05-25 18:48:17 +0900

    header 1차 스타일 수정, primary color 설정

--- community/src/components/home/Search.vue
+++ community/src/components/home/Search.vue
@@ -1,23 +1,12 @@
 <script lang="ts">
-import { PopularSearchEntity } from '@/api/dto/popularSearch.dto';
 import ApocImageSet from '@/components/common/ApocImageSet.vue';
 import ApocInput from '@/components/common/ApocInput.vue';
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Search',
   components: { ApocInput, ApocImageSet },
-  setup() {
-    const popularKeywordList = ref<PopularSearchEntity[]>([]);
-    const videoSrcPc = ref<string>('/assets/videos/search_background_video_v2.mp4');
-    const videoSrcTablet = ref<string>('/assets/videos/search_background_video_v2_tablet.mp4');
-    
-    return {
-      videoSrcPc,
-      videoSrcTablet,
-      popularKeywordList,
-    };
-  },
+
 });
 </script>
 
@@ -31,20 +20,8 @@ export default defineComponent({
         </div>
       </section>
     </section>
-    <!-- 인기 검색어 영역 위치 검색 결과 화면 나온 후 작업 -->
-<!--    <section class="popular-keyword-section">-->
-<!--      <div class="title">인기 검색어</div>-->
-<!--      <div class="content">-->
-<!--        <span @click="upComingTask(item.popularSearchKrName)" v-for="item in popularKeywordList" :key="item.popularSearchIdx">-->
-<!--          {{ item.popularSearchKrName }}-->
-<!--        </span>-->
-<!--      </div>-->
-<!--    </section>-->
-    <video class="search-background pc" v-if="videoSrcPc" loop autoplay muted playsinline>
-      <source :src="videoSrcPc" type="video/mp4" />
-    </video>
-		<video class="search-background tablet" v-if="videoSrcTablet" loop autoplay muted playsinline>
-      <source :src="videoSrcTablet" type="video/mp4" />
+      <div>search section</div>
+    <video class="search-background pc" xloop autoplay muted playsinline>
     </video>
   </div>
 </template>
