a3d131577e92a69edc408497394243ac6b28f9a9
mmyonaa <apddfhsajrwk@gmail.com>
2025-06-03 18:00:02 +0900

    오시는 길 페이지

--- community/src/views/IntroduceRoute.vue
+++ community/src/views/IntroduceRoute.vue
@@ -22,24 +22,35 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="home-page">
-    <section class="search-section">
-      <search />
-    </section>
-    <section class="tutorial-section">
-      <tutorial />
-    </section>
-    <section class="news-section">
-      <news />
-    </section>
-    <div class="popular-section">
-      <popular />
+  <div class="page-common introduce-page route-page">
+    <h1>오시는 길</h1>
+    <div class="wrapper">
+      <div class="wrapper-item">
+      <div class="theater-img">
+          <img src="/assets/images/theater/introduce-1.JPG" />
+      </div>
+      <div class="text">
+       <h2>대중교통</h2>
+<h3>지하철 이용</h3>
+<ul>
+  <li>• 한남역 1번 출구 – 도보 21분</li>
+  <li>• 이태원역 4번 출구 – 도보 20분</li>
+</ul>
+<h3>버스 이용</h3>
+<ul>
+  <li>• 이태원 방면) 🚌 400 / 405 / 421</li>
+  <li class="indent">‘보광동주민센터’ 정류장 하차 - 도보 5분</li>
+  <li>• 한남대교 방면) 🚌 400 / 405 / 421</li>
+  <li class="indent">‘보광동신동아아파트’ 정류장 하차 - 도보 6분</li>
+  <li class="indent">🚌 2016 / 6211</li>
+  <li class="indent">‘보광신동아아파트.삼성리버빌아파트’ 정류장 하차 - 도보 8분</li>
+  <li class="indent">🚌 용산 01번</li>
+  <li class="indent">‘보광동주민센터’ 정류장 하차 후 언덕길 도보 4분</li>
+</ul>
+<h2>차량</h2>
+주차 불가 
+</div>
     </div>
-    <div class="notice-section">
-      <notice />
-    </div>
-    <div class="instagram-section">
-      <instagram />
-    </div>
+  </div>
   </div>
 </template>
