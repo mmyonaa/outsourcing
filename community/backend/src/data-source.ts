d2490531445a4fbd02451e0671133aad1c489755
mmyonaa <apddfhsajrwk@gmail.com>
2025-07-20 19:37:37 +0900

    db env 변수명 통일

--- community/backend/src/data-source.ts
+++ community/backend/src/data-source.ts
@@ -9,7 +9,7 @@ export const AppDataSource = new DataSource({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   synchronize: true, // 운영 시 false로 변경!
   entities: [User],
