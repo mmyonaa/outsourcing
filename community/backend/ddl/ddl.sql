60f378cb5ce8fd16f9a9040c829cc4539498d491
mmyonaa <apddfhsajrwk@gmail.com>
2025-07-20 18:03:13 +0900

    ddl 생성

---
+++ community/backend/ddl/ddl.sql
@@ -0,0 +1,27 @@
+-- 확장 설치 (최초 1회만 실행)
+-- CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+-- 공지사항
+CREATE TABLE board (
+    board_idx UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    title VARCHAR(500),
+    body TEXT,
+    views INTEGER DEFAULT 0,
+    best_yn CHAR(1) DEFAULT 'N',
+    reg_dt TIMESTAMP,
+    mod_dt TIMESTAMP,
+    del_yn CHAR(1) DEFAULT 'N'
+);
+
+-- 공연
+CREATE TABLE performance (
+    per_idx UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    per_type VARCHAR(50),
+    title VARCHAR(500),
+    body TEXT,
+    img_url VARCHAR(100),
+    views INTEGER DEFAULT 0,
+    reg_dt TIMESTAMP,
+    mod_dt TIMESTAMP,
+    del_yn CHAR(1) DEFAULT 'N'
+);
