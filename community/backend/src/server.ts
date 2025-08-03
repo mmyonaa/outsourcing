21c36778d3611ec2fa016cf852a0a388f46c0514
mmyonaa <apddfhsajrwk@gmail.com>
2025-08-03 22:15:46 +0900

    server 규칙 수정

--- community/backend/src/server.ts
+++ community/backend/src/server.ts
@@ -28,6 +28,6 @@ fs.readdirSync(routesPath)
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on http://0.0.0.0:3000');
 });
