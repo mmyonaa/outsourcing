8f68b150fc114196ccaf3a4b9e7c0a2697f9eb15
mmyonaa <apddfhsajrwk@gmail.com>
2025-08-30 22:17:01 +0900

    constants 수정

--- community/frontend/src/constants.ts
+++ community/frontend/src/constants.ts
@@ -8,8 +8,9 @@ export const APP_ENV_TYPE = {
 
 const AppConfig = {
   ENV: APP_ENV_TYPE.DEV,
+  API_SERVER: '',
   // API_SERVER: 'http://localhost:3000',
-  API_SERVER: 'http://ec2-13-125-101-232.ap-northeast-2.compute.amazonaws.com:3000',
+  // API_SERVER: 'http://ec2-13-125-101-232.ap-northeast-2.compute.amazonaws.com:3000',
   FRONT_HOST: 'http://localhost:4000',
   FILE_SERVER: 'https://apoc-community.s3.ap-northeast-2.amazonaws.com/',
   KEYS,
