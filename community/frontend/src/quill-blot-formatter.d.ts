c8b74a5cf7bbd3b5bc00883832e2e576833f0139
mmyonaa <apddfhsajrwk@gmail.com>
2025-10-20 09:25:00 +0900

    빌드 오류 수정

---
+++ community/frontend/src/quill-blot-formatter.d.ts
@@ -0,0 +1,7 @@
+declare module 'quill-blot-formatter/dist/BlotFormatter' {
+  import Quill from 'quill';
+
+  export default class BlotFormatter {
+    constructor(quill: Quill, options?: any);
+  }
+}
